5eaa0cb0508092d1109e33124eee3bc4fed070a6
Siddhant Goel <siddhantgoel@gmail.com>
2017-02-18 09:36:26 +0100

    move finder into core.finder

--- setup.py
+++ setup.py
@@ -1,41 +1,36 @@
-from setuptools import setup, Extension
+from setuptools import setup, Extension, find_packages
 import sys
-import warnings
 
 
 with open('README.rst') as f:
-    description = f.read()
+    long_description = f.read()
 
 
 cythonize = None
 
 
 try:
-    sys.argv.remove('--using-cython')
-except ValueError:
+    from Cython.Build import cythonize
+except ImportError:
     pass
-else:
-    try:
-        from Cython.Build import cythonize
-    except ImportError:
-        warnings.warn('Cython not installed')
 
 
 if cythonize:
-    extensions = cythonize('streaming_form_data/finder.pyx')
+    extensions = cythonize('streaming_form_data/core/finder.pyx')
 else:
-    extensions = [Extension('streaming_form_data.finder',
-                            ['streaming_form_data/finder.c'])]
+    extensions = [Extension('streaming_form_data.core.finder',
+                            ['streaming_form_data/core/finder.c'])]
 
 
 setup(
     name='streaming_form_data',
     version='0.2.0',
-    description=description,
+    description='Streaming parser for multipart/form-data',
+    long_description=long_description,
     author='Siddhant Goel',
     author_email='siddhantgoel@gmail.com',
     license='MIT',
     url='https://github.com/siddhantgoel/streaming-form-data',
-    packages=['streaming_form_data'],
+    packages=find_packages(exclude=['examples', 'tests', 'utils']),
     ext_modules=extensions
 )
