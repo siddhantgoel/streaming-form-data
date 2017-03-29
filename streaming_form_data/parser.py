8c79813dd7f1089b4202265a7f35166ea7ffa5ea
Siddhant Goel <siddhantgoel@gmail.com>
2017-03-29 21:46:57 +0200

    remove extra namedtuple function calls

--- streaming_form_data/parser.py
+++ streaming_form_data/parser.py
@@ -1,4 +1,3 @@
-from collections import namedtuple
 import cgi
 import enum
 
@@ -51,9 +50,6 @@ def parse_content_boundary(headers):
         raise ParseFailedException('Boundary not found')
 
     return boundary.encode('utf-8')
-
-
-Position = namedtuple('Position', ['buffer_start', 'buffer_end'])
 
 
 class StreamingFormDataParser(object):
@@ -112,39 +108,8 @@ class StreamingFormDataParser(object):
         self._parse(chunk, index, buffer_start, buffer_end)
 
     def _parse(self, chunk, index, buffer_start, buffer_end):
-        position = Position(buffer_start, buffer_end)
-
-        def expand_buffer():
-            return position._replace(buffer_end=position.buffer_end+1)
-
         def buffer_length():
-            return position.buffer_end - position.buffer_start
-
-        def reset_buffer():
-            return position._replace(buffer_start=index+1,
-                                     buffer_end=index+1)
-
-        def truncate_buffer(suffix):
-            if buffer_length() <= len(suffix):
-                return position
-
-            idx = position.buffer_end - len(suffix)
-
-            self._active_part.data_received(
-                chunk[position.buffer_start: idx - 2])
-
-            return reset_buffer()
-
-        def try_flush_buffer():
-            if buffer_length() <= self._max_buffer_size:
-                return position
-
-            index = position.buffer_end - 1
-
-            self._active_part.data_received(
-                chunk[position.buffer_start: index])
-
-            return position._replace(buffer_start=index)
+            return buffer_end - buffer_start
 
         while index < len(chunk):
             byte = chunk[index]
@@ -153,16 +118,16 @@ class StreamingFormDataParser(object):
                 if byte != HYPHEN:
                     raise ParseFailedException()
 
-                position = expand_buffer()
+                buffer_end += 1
                 self.state = ParserState.STARTING_BOUNDARY
             elif self.state == ParserState.STARTING_BOUNDARY:
                 if byte != HYPHEN:
                     raise ParseFailedException()
 
-                position = expand_buffer()
+                buffer_end += 1
                 self.state = ParserState.READING_BOUNDARY
             elif self.state == ParserState.READING_BOUNDARY:
-                position = expand_buffer()
+                buffer_end += 1
 
                 if byte == CR:
                     self.state = ParserState.ENDING_BOUNDARY
@@ -170,24 +135,24 @@ class StreamingFormDataParser(object):
                 if byte != LF:
                     raise ParseFailedException()
 
-                position = expand_buffer()
+                buffer_end += 1
 
                 if buffer_length() < 4:
                     return False
 
-                indices = (position.buffer_start,
-                           position.buffer_start + 1,
-                           position.buffer_end - 1,
-                           position.buffer_end - 2)
+                indices = (buffer_start,
+                           buffer_start + 1,
+                           buffer_end - 1,
+                           buffer_end - 2)
 
                 if all([chunk[idx] == HYPHEN for idx in indices]):
                     self.state = ParserState.END
 
-                position = reset_buffer()
+                buffer_start = buffer_end = index + 1
 
                 self.state = ParserState.READING_HEADER
             elif self.state == ParserState.READING_HEADER:
-                position = expand_buffer()
+                buffer_end += 1
 
                 if byte == CR:
                     self.state = ParserState.ENDING_HEADER
@@ -195,9 +160,9 @@ class StreamingFormDataParser(object):
                 if byte != LF:
                     raise ParseFailedException()
 
-                position = expand_buffer()
+                buffer_end += 1
 
-                header = chunk[position.buffer_start: position.buffer_end]
+                header = chunk[buffer_start: buffer_end]
 
                 value, params = cgi.parse_header(header.decode('utf-8'))
 
@@ -207,7 +172,7 @@ class StreamingFormDataParser(object):
 
                     self._set_active_part(part)
 
-                position = reset_buffer()
+                buffer_start = buffer_end = index + 1
 
                 self.state = ParserState.ENDED_HEADER
             elif self.state == ParserState.ENDED_HEADER:
@@ -216,32 +181,54 @@ class StreamingFormDataParser(object):
                 else:
                     self.state = ParserState.READING_HEADER
 
-                position = expand_buffer()
+                buffer_end += 1
             elif self.state == ParserState.ENDING_ALL_HEADERS:
                 if byte != LF:
                     raise ParseFailedException()
 
-                position = reset_buffer()
+                buffer_start = buffer_end = index + 1
                 self.state = ParserState.READING_BODY
             elif self.state == ParserState.READING_BODY:
-                position = expand_buffer()
+                buffer_end += 1
 
                 self._delimiter_finder.feed(byte)
                 self._ender_finder.feed(byte)
 
                 if self._delimiter_finder.found:
                     self.state = ParserState.READING_HEADER
-                    position = truncate_buffer(self._delimiter)
+
+                    if buffer_length() > len(self._delimiter):
+                        idx = buffer_end - len(self._delimiter)
+
+                        self._active_part.data_received(
+                            chunk[buffer_start: idx - 2])
+
+                        buffer_start = buffer_end = index + 1
+
                     self._unset_active_part()
                     self._delimiter_finder.reset()
                 elif self._ender_finder.found:
                     self.state = ParserState.END
-                    position = truncate_buffer(self._ender)
+
+                    if buffer_length() > len(self._ender):
+                        idx = buffer_end - len(self._ender)
+
+                        self._active_part.data_received(
+                            chunk[buffer_start: idx - 2])
+
+                        buffer_start = buffer_end = index + 1
+
                     self._ender_finder.reset()
                 else:
                     if self._ender_finder.inactive and \
-                            self._delimiter_finder.inactive:
-                        position = try_flush_buffer()
+                            self._delimiter_finder.inactive and \
+                            buffer_length() > self._max_buffer_size:
+                        idx = buffer_end - 1
+
+                        self._active_part.data_received(
+                            chunk[buffer_start: idx])
+
+                        buffer_start = index
             elif self.state == ParserState.END:
                 return
             else:
@@ -250,8 +237,7 @@ class StreamingFormDataParser(object):
             index += 1
 
         if buffer_length() > 0:
-            self._leftover_buffer = \
-                chunk[position.buffer_start: position.buffer_end]
+            self._leftover_buffer = chunk[buffer_start: buffer_end]
 
     def _part_for(self, name):
         for part in self.expected_parts:
