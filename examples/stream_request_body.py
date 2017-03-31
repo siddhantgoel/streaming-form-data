6db66a555130216b637057b69c5c0a25751751a6
Siddhant Goel <siddhantgoel@gmail.com>
2017-03-31 10:09:06 +0200

    update usage example

--- examples/stream_request_body.py
+++ examples/stream_request_body.py
@@ -1,5 +1,3 @@
-import os.path
-
 from tornado.web import stream_request_body, RequestTarget
 
 from streaming_form_data.targets import ValueTarget, FileTarget
@@ -10,16 +8,22 @@ from streaming_form_data.part import Part
 @stream_request_body
 class UploadTarget(RequestTarget):
     def prepare(self):
+        self.value = ValueTarget()
+        self.file_ = FileTarget('/tmp/image.png')
+
         expected_parts = (
-            Part('name', ValueTarget()),
-            Part('image', FileTarget(os.path.join('/tmp', 'image.png'))),
+            Part('name', self.value),
+            Part('image', self.file_),
         )
 
-        self._parser = StreamingFormDataParser(expected_parts,
-                                               headers=self.request.headers)
+        self._parser = StreamingFormDataParser(
+            expected_parts, headers=self.request.headers)
 
     def data_received(self, chunk):
         self._parser.data_received(chunk)
 
     def post(self):
+        print('name: {}'.format(self.value))
+        print('image: {}'.format(self.file_.name))
+
         self.finish()
