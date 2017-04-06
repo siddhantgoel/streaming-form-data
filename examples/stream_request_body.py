12ef3cddfd809612d5d0e298df80994076ca2afb
Siddhant Goel <siddhantgoel@gmail.com>
2017-04-06 21:41:01 +0200

    update tornado usage example

--- examples/stream_request_body.py
+++ examples/stream_request_body.py
@@ -1,18 +1,22 @@
-from tornado.web import stream_request_body, RequestHandler
+import os.path
+from time import time
+
+from tornado.web import Application, RequestHandler, stream_request_body
+from tornado.ioloop import IOLoop
 
 from streaming_form_data import StreamingFormDataParser, Part
 from streaming_form_data.targets import ValueTarget, FileTarget
 
 
 @stream_request_body
-class UploadTarget(RequestHandler):
+class UploadHandler(RequestHandler):
     def prepare(self):
         self.value = ValueTarget()
-        self.file_ = FileTarget('/tmp/image.png')
+        self.file_ = FileTarget('/tmp/file-{}.dat'.format(int(time())))
 
         expected_parts = (
             Part('name', self.value),
-            Part('image', self.file_),
+            Part('file', self.file_),
         )
 
         self._parser = StreamingFormDataParser(
@@ -22,7 +26,34 @@ class UploadTarget(RequestHandler):
         self._parser.data_received(chunk)
 
     def post(self):
-        print('name: {}'.format(self.value))
-        print('image: {}'.format(self.file_.name))
+        print('name: {}'.format(self.value.value))
+        print('file: {}'.format(self.file_.filename))
 
         self.finish()
+
+
+class IndexHandler(RequestHandler):
+    def get(self):
+        self.render('index.html')
+
+
+def main():
+    handlers = [
+        (r'/', IndexHandler),
+        (r'/upload', UploadHandler),
+    ]
+
+    settings = dict(
+        debug=True,
+        template_path=os.path.dirname(__file__)
+    )
+
+    app = Application(handlers, **settings)
+    app.listen(9999)
+
+    IOLoop().current().start()
+
+
+if __name__ == '__main__':
+    print('Listening on port 9999')
+    main()
