a2ec59b90e7fcb62dd73bf846b7c4be147ce11b2
Siddhant Goel <me@sgoel.org>
2019-07-31 20:21:24 -0400

    adjust poetry build script

--- build.py
+++ build.py
@@ -1,7 +1,24 @@
-from Cython.Build import cythonize
+from setuptools import Extension
+
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    USE_CYTHON = False
+else:
+    USE_CYTHON = True
 
 
 def build(setup_kwargs):
-    setup_kwargs.update(
-        {'ext_modules': cythonize(['streaming_form_data/_parser.pyx'])}
-    )
+    file_ext = 'pyx' if USE_CYTHON else 'c'
+
+    extensions = [
+        Extension(
+            'streaming_form_data._parser',
+            ['streaming_form_data/_parser.{}'.format(file_ext)],
+        )
+    ]
+
+    if USE_CYTHON:
+        extensions = cythonize(extensions)
+
+    setup_kwargs.update({'ext_modules': extensions})
