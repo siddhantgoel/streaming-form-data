9b4db24e6bec35cdfd0dfc313bafceba5a0d3da4
Siddhant Goel <me@sgoel.org>
2020-01-28 10:27:17 +0100

    use pytest for exception tests

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,4 +1,4 @@
-from unittest import TestCase
+import pytest
 
 from requests_toolbelt import MultipartEncoder
 
@@ -6,31 +6,22 @@ from streaming_form_data import StreamingFormDataParser
 from streaming_form_data.targets import ValueTarget
 
 
-def simple_python_function(chunk):
-    raise ValueError('CustomTarget exception')
-
-
-class ExceptionTestCase(TestCase):
-    def test_basic_single(self):
-        self.assertRaises(ValueError, simple_python_function, "abcd")
-
-
 class CustomTarget(ValueTarget):
     def data_received(self, chunk):
         raise ValueError('CustomTarget exception')
 
 
-class ParserTargetExceptionTestCase(TestCase):
-    def test_basic_single(self):
-        target = CustomTarget()
+def test_custom_target_exception():
+    target = CustomTarget()
 
-        encoder = MultipartEncoder(fields={'value': 'hello world'})
+    encoder = MultipartEncoder(fields={'value': 'hello world'})
 
-        parser = StreamingFormDataParser(
-            headers={'Content-Type': encoder.content_type}
-        )
-        parser.register('value', target)
+    parser = StreamingFormDataParser(
+        headers={'Content-Type': encoder.content_type}
+    )
+    parser.register('value', target)
 
-        data = encoder.to_string()
+    data = encoder.to_string()
 
-        self.assertRaises(ValueError, parser.data_received, data)
+    with pytest.raises(ValueError):
+        parser.data_received(data)
