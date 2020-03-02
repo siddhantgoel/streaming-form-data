4a7fb3c749c2263de0288610af542e3f608ecc07
Siddhant Goel <me@sgoel.org>
2020-03-02 17:16:35 +0530

    tests: additional test case for max size validator

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -3,7 +3,14 @@ import pytest
 from streaming_form_data.validators import MaxSizeValidator, ValidationError
 
 
-def test_max_size():
+def test_max_size_validator_empty_input():
+    validator = MaxSizeValidator(0)
+
+    with pytest.raises(ValidationError):
+        validator('x')
+
+
+def test_max_size_validator_normal():
     validator = MaxSizeValidator(5)
 
     for char in 'hello':
