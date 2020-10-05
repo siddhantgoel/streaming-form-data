71cbede39e014c088e817a86eef0db9fdf01310e
Siddhant Goel <me@sgoel.dev>
2020-10-05 20:33:22 +0200

    drop python 3.5 support

--- setup.py
+++ setup.py
@@ -34,12 +34,11 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
         'Programming Language :: Cython',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3 :: Only',
     ],
     keywords='form-data, forms, http, multipart, web',
-    python_requires='>=3.5',
+    python_requires='>=3.6',
 )
