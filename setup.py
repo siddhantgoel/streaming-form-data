27b58cfa91ce2c1b7312f67f3fde2fd0ecc95056
Siddhant Goel <me@sgoel.dev>
2022-10-06 20:50:53 +0200

    deploy: bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(here.joinpath('README.md')) as fd:
 
 setup(
     name='streaming-form-data',
-    version='1.10.2',
+    version='1.11.0',
     description='Streaming parser for multipart/form-data',
     long_description=long_description,
     long_description_content_type='text/markdown',
