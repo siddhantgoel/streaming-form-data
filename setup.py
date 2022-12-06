e4a7442feda0f6964398c4a3b717adb319857520
Siddhant Goel <me@sgoel.dev>
2022-12-06 19:35:31 +0100

    chore: drop python 3.7

--- setup.py
+++ setup.py
@@ -35,12 +35,11 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
         'Programming Language :: Cython',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3 :: Only',
     ],
     keywords='form-data, forms, http, multipart, web',
-    python_requires='>=3.6',
+    python_requires='>=3.8',
 )
